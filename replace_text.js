7633ffedf6919c94a0ebaf15cee56e5c57e2f8ce
Richard Green <vanchagreen@gmail.com>
2015-03-23 00:35:58 -0600

    Fixed #1

--- replace_text.js
+++ replace_text.js
@@ -1,8 +1,13 @@
-var sel = window.getSelection();
-var range;
-
-if (sel.rangeCount) {
-  range = sel.getRangeAt(0);
-  range.deleteContents();
-  range.insertNode(document.createTextNode(plainText))
+if (document.activeElement.nodeName === "TEXTAREA" || document.activeElement.nodeName === "INPUT") {
+  var ta = document.activeElement;
+  ta.value = ta.value.slice(0, ta.selectionStart) + plainText + ta.value.slice(ta.selectionEnd, ta.length);
+}
+else {
+  var sel = window.getSelection();
+  var range;
+  if (sel.rangeCount) {
+    range = sel.getRangeAt(0);
+    range.deleteContents();
+    range.insertNode(document.createTextNode(plainText))
+  }
 }
